53d097dc3712c0340d1d835dd35539714112faa9
Anna Lazarenko <aulazar@yandex.ru>
2024-11-30 23:45:45 +0700

    fix: fix scroll when burger-menu open

--- christmas-shop/js/main.js
+++ christmas-shop/js/main.js
@@ -1,4 +1,5 @@
 function burgerMenu() {
     document.getElementById('burger-menu').classList.toggle('show_menu')
     document.getElementById('burger-button').classList.toggle('burger-rotated')
+    document.getElementsByTagName("body")[0].classList.toggle('body-fixed')
 }
