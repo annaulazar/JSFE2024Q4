114b7f6b67e3e7ab4251450225ae5b4f543b4acc
Anna Lazarenko <aulazar@yandex.ru>
2024-12-02 22:44:18 +0700

    feat: add cards filter on gifts page

--- christmas-shop/js/gifts.js
+++ christmas-shop/js/gifts.js
@@ -10,7 +10,7 @@ const tags = {
 
 function createCard(cardObj) {
     const card = document.createElement('div');
-    card.classList = ['gifts__cards-item', 'card']
+    card.classList = 'gifts__cards-item card'
     card.classList.add(tags[cardObj.category])
     const content = `<div class="card-image"></div>
                     <div class="card-text">
@@ -32,7 +32,13 @@ function createCards(cardsList) {
     }
 }
 function updateCards(filter) {
-
+    cardsItems.innerHTML = ''
+    if (filter === 'ALL') {
+        createCards(allCards)
+    } else {
+        let filterCards = allCards.filter((elem) => elem.querySelector('.card-tag').innerText.toUpperCase() === filter)
+        createCards(filterCards)
+    }
 }
 function cardsFilter(e) {
     let target = e.target;
