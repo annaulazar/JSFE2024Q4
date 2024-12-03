23b7683377e5003a874c3c9779cf0ba4e5c60c8b
Anna Lazarenko <aulazar@yandex.ru>
2024-12-03 23:44:37 +0700

    feat: add slider on home page

--- christmas-shop/js/home.js
+++ christmas-shop/js/home.js
@@ -5,6 +5,11 @@ const hours = document.querySelector('#hours')
 const minutes = document.querySelector('#minutes')
 const seconds = document.querySelector('#seconds')
 const cardsItems = document.querySelector('.gifts__cards');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+const slider = document.querySelector('.slider-row');
+const sliderRowWidth = 1995;
+let cntSteps = 0;
 
 
 function timerNewYear() {
@@ -54,7 +59,60 @@ function addRandomCards(cnt, array) {
     }
 }
 
+function getSliderStep() {
+    let step;
+    let containerWidth = document.querySelector('.slider__content').clientWidth;
+    let windowWidth = window.innerWidth;
+    if (windowWidth > 768) {
+        step = Math.ceil((sliderRowWidth - containerWidth) / 3)
+        return {step: step, maxcnt: 3}
+    } else {
+        step = Math.ceil((sliderRowWidth - containerWidth) / 6)
+        return {step: step, maxcnt: 6}
+    }
+}
+
+function moveSliderLeft() {
+    let {step, maxcnt} = getSliderStep();
+    cntSteps--;
+    let deltaX = -(step * cntSteps)
+    slider.style.transform = `translate(${deltaX}px, 0)`;
+    if (cntSteps === maxcnt - 1) {
+        arrowRight.removeAttribute('disabled')
+    }
+    if (cntSteps === 0) {
+        arrowLeft.setAttribute('disabled', null)
+    }
+
+}
+
+function moveSliderRight() {
+    let {step, maxcnt} = getSliderStep();
+    cntSteps++;
+    let deltaX = -(step * cntSteps)
+    slider.style.transform = `translate(${deltaX}px, 0)`;
+    if (cntSteps === 1) {
+        arrowLeft.removeAttribute('disabled')
+    }
+    if (cntSteps === maxcnt) {
+        arrowRight.setAttribute('disabled', null)
+    }
+}
+
+function resetSlider() {
+    cntSteps = 0;
+    slider.style.transform = `translate(0, 0)`;
+    if (!arrowLeft.hasAttribute('disabled')) {
+        arrowLeft.setAttribute('disabled', null)
+    }
+    if (arrowRight.hasAttribute('disabled')) {
+        arrowRight.removeAttribute('disabled')
+    }
+}
 
 addRandomCards(4, GIFTS)
 timerNewYear()
 setInterval(timerNewYear, 1000)
+arrowLeft.addEventListener('click', moveSliderLeft)
+arrowRight.addEventListener('click', moveSliderRight)
+window.addEventListener('resize', resetSlider)
